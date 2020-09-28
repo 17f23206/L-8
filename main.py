962672b05d89f2f5615bac27b6a498216a805273
Jamie McNally <17f23206@marling.school>
2020-09-28 11:17:29 +0000

    favourite dog

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
-print("Enter a number: ")
-odd_even = int(input())
-odd_even = odd_even % 2
-if odd_even == 1:
-    print("Your number is odd")
-elif odd_even == 0:
-    print("Your number is even")
-
-
+print("What is your name?")
+name = input().lower()
+if name == "anakin":
+  print("How do you do Anakin!")
+else:
+  print(f"Nice name, {name}")
+print(f"So {name}, is it hot or cold where you are today?")
+weather = input().upper()
+if weather == "COLD":
+  print("You must be freezing!")
+elif weather == "HOT":
+  print("Drink plenty of water")
+else:
+  print("I can't advise you on that type of weather.")
+print("Do you like the colour blue?")
+likes_blue = input().lower()
+if likes_blue == "Yes":
+  print("I like blue too")
+print("Have a good day! Bye!")
+print("wait, what is your favourite dog?")
+dog = input().lower()
+if dog == "retriever":
+  print("that is my favourite dog too!")
