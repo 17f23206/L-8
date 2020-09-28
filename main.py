33cc7de9760e836709ff05693ca1dc19096e22d4
Jamie McNally <17f23206@marling.school>
2020-09-28 10:54:03 +0000

    odd or even

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-print("Guess a number between 1 and 10:")
-number = int(input())
-if number == 7:
-    print("You got it!")
-elif number < 7:
-    print("Higher")
-else:
-    print("Lower")
+print("Enter a number: ")
+odd_even = int(input())
+odd_even = odd_even % 2
+if odd_even == 1:
+    print("Your number is odd")
+elif odd_even == 0:
+    print("Your number is even")
+
 
