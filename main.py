2d2735fac6ff981340bb5ebc90502c39afbdccca
Jamie McNally <17f23206@marling.school>
2020-09-28 10:51:02 +0000

    higher or lower

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ print("Guess a number between 1 and 10:")
 number = int(input())
 if number == 7:
     print("You got it!")
+elif number < 7:
+    print("Higher")
 else:
-    print("Incorrect")
+    print("Lower")
+
